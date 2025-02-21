28681d71eb7f470ee2c32fa4e3873d663e206a27
JoeMinKyung <mk981015@naver.com>
2025-02-21 21:38:09 +0900

    init

---
+++ src/test/java/com/example/basicsession/BasicSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
