4f340b0a05361d30721f941f9429653363354d01
JoeMinKyung <mk981015@naver.com>
2025-02-21 21:41:46 +0900

    feat: save, getAll, getOne, update, delete

---
+++ src/main/java/com/example/basicsession/member/controller/MemberController.java
@@ -0,0 +1,44 @@
+package com.example.basicsession.member.controller;
+
+import com.example.basicsession.member.dto.MemberResponseDto;
+import com.example.basicsession.member.dto.MemberSaveRequestDto;
+import com.example.basicsession.member.dto.MemberSaveResponseDto;
+import com.example.basicsession.member.dto.MemberUpdateRequestDto;
+import com.example.basicsession.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/members")
+    public ResponseEntity<MemberSaveResponseDto> save(@RequestBody MemberSaveRequestDto dto) {
+        return ResponseEntity.ok(memberService.save(dto));
+    }
+
+    @GetMapping("/members")
+    public ResponseEntity<List<MemberResponseDto>> getAll() {
+        return ResponseEntity.ok(memberService.findAll());
+    }
+
+    @GetMapping("/members/{memberId}")
+    public ResponseEntity<MemberResponseDto> getOne(@PathVariable Long memberId) {
+        return ResponseEntity.ok(memberService.findById(memberId));
+    }
+
+    @PutMapping("/members/{memberId}")
+    public void update(@PathVariable Long memberId, @RequestBody MemberUpdateRequestDto dto) {
+        memberService.update(memberId, dto);
+    }
+
+    @DeleteMapping("/members/{memberId}")
+    public void delete(@PathVariable Long memberId) {
+        memberService.deleteById(memberId);
+    }
+}
---
+++ src/main/java/com/example/basicsession/member/service/MemberService.java
@@ -0,0 +1,61 @@
+package com.example.basicsession.member.service;
+
+import com.example.basicsession.member.dto.MemberResponseDto;
+import com.example.basicsession.member.dto.MemberSaveRequestDto;
+import com.example.basicsession.member.dto.MemberSaveResponseDto;
+import com.example.basicsession.member.dto.MemberUpdateRequestDto;
+import com.example.basicsession.member.entity.Member;
+import com.example.basicsession.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public MemberSaveResponseDto save(MemberSaveRequestDto dto) {
+
+        Member member = new Member(dto.getEmail());
+        Member savedMember = memberRepository.save(member);
+        return new MemberSaveResponseDto(
+                savedMember.getId(),
+                savedMember.getEmail()
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public List<MemberResponseDto> findAll() {
+        List<Member> members = memberRepository.findAll();
+
+        return members.stream().map(member -> new MemberResponseDto(member.getId(), member.getEmail())).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public MemberResponseDto findById(Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new IllegalStateException("그런 사람 없음."));
+        return new MemberResponseDto(
+                member.getId(),
+                member.getEmail()
+        );
+    }
+
+    @Transactional
+    public void update(Long memberId, MemberUpdateRequestDto dto) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new IllegalStateException("그런 사람 없음."));
+        member.update(dto.getEmail());
+    }
+
+    @Transactional
+    public void deleteById(Long memberId) {
+        if(!memberRepository.existsById(memberId)) {
+            throw new IllegalStateException("그런 사람 없음.");
+        }
+        memberRepository.deleteById(memberId);
+    }
+}
