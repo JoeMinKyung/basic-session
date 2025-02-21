4a5a6ca5d17c7bb72cd0e26a1573dd57cf3cca63
JoeMinKyung <mk981015@naver.com>
2025-02-21 21:54:57 +0900

    feat: findAll

--- src/main/java/com/example/basicsession/todo/controller/TodoController.java
+++ src/main/java/com/example/basicsession/todo/controller/TodoController.java
@@ -1,13 +1,17 @@
 package com.example.basicsession.todo.controller;
 
+import com.example.basicsession.todo.dto.TodoResponseDto;
 import com.example.basicsession.todo.dto.TodoSaveRequestDto;
 import com.example.basicsession.todo.dto.TodoSaveResponseDto;
 import com.example.basicsession.todo.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +22,10 @@ public class TodoController {
     @PostMapping("/todos")
     public ResponseEntity<TodoSaveResponseDto> save(@RequestBody TodoSaveRequestDto dto) {
         return ResponseEntity.ok(todoService.save(dto));
+    }
+
+    @GetMapping("/todos")
+    public ResponseEntity<List<TodoResponseDto>> getAll() {
+        return ResponseEntity.ok(todoService.findAll());
     }
 }
--- src/main/java/com/example/basicsession/todo/service/TodoService.java
+++ src/main/java/com/example/basicsession/todo/service/TodoService.java
@@ -1,11 +1,16 @@
 package com.example.basicsession.todo.service;
 
+import com.example.basicsession.todo.dto.TodoResponseDto;
 import com.example.basicsession.todo.dto.TodoSaveRequestDto;
 import com.example.basicsession.todo.dto.TodoSaveResponseDto;
 import com.example.basicsession.todo.entity.Todo;
 import com.example.basicsession.todo.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +18,7 @@ public class TodoService {
 
     private final TodoRepository todoRepository;
 
+    @Transactional
     public TodoSaveResponseDto save(TodoSaveRequestDto dto) {
         Todo todo = new Todo(dto.getContent());
         Todo savedTodo = todoRepository.save(todo);
@@ -20,5 +26,18 @@ public class TodoService {
                 savedTodo.getId(),
                 savedTodo.getContent()
         );
+    }
+
+    @Transactional(readOnly = true)
+    public List<TodoResponseDto> findAll() {
+        List<Todo> todos = todoRepository.findAll();
+        List<TodoResponseDto> dtos = new ArrayList<>();
+        for (Todo todo : todos) {
+            dtos.add(new TodoResponseDto(
+                    todo.getId(),
+                    todo.getContent()
+            ));
+        }
+        return dtos;
     }
 }
