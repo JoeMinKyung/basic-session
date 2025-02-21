f21ea761a81fe07578049a7ff86061b3e711aaa8
JoeMinKyung <mk981015@naver.com>
2025-02-21 21:41:06 +0900

    feat: MemberSaveResponseDto 생성

---
+++ src/main/java/com/example/basicsession/member/dto/MemberSaveResponseDto.java
@@ -0,0 +1,12 @@
+package com.example.basicsession.member.dto;
+
+public class MemberSaveResponseDto {
+
+    private final Long id;
+    private final String email;
+
+    public MemberSaveResponseDto(Long id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+}
